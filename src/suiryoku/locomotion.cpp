abdf59ac3049c183c013a81f0cd046709c0a1063
maroqijalil <jalilpro31@gmail.com>
2021-06-07 23:23:46 +0700

    feat: add cmake

--- src/suiryoku/locomotion.cpp
+++ src/suiryoku/locomotion.cpp
@@ -18,6 +18,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+#include <unistd.h>
+
 #include <aruku/walking.hpp>
 #include <kansei/imu.hpp>
 #include <suiryoku/locomotion.hpp>
