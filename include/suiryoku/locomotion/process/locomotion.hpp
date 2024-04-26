9611360f201c42cd19ef05c053d6f013534e2b64
FaaizHaikal <faaizhaikal@gmail.com>
2024-04-26 21:32:31 +0700

    refactor: expose config_name

--- include/suiryoku/locomotion/process/locomotion.hpp
+++ include/suiryoku/locomotion/process/locomotion.hpp
@@ -36,7 +36,6 @@ class Locomotion
 public:
   explicit Locomotion(std::shared_ptr<Robot> robot);
 
-  void set_config_name(const std::string & name) {config_name = name;}
   void load_config(const std::string & path);
   void set_config(const nlohmann::json & json);
 
@@ -80,8 +79,9 @@ public:
   std::function<void()> stop;
   std::function<void()> start;
 
-private:
   std::string config_name;
+
+private:
 
   double move_min_x;
   double move_max_x;
